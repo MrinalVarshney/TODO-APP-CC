e4c491fdbfc6f931b231034cfa8a52a395779c39
MrinalVarshney <varshneymrinal2005@gmail.com>
2024-12-26 18:22:53 +0000

    Fixed:Add task API endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,7 +31,7 @@ const App = () => {
   const addTodo = () => {
     if (newTodo.trim() === '') return; // Prevent adding empty todos
     
-    axios.post(`${process.env.REACT_APP_SERVER_BASE_URL}/api/todos`, {
+    axios.post(`${process.env.REACT_APP_SERVER_BASE_URL}/`, {
       text: newTodo,
     })
       .then((response) => {
